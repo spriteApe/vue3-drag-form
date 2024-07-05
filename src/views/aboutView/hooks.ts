05d6814191d42f613c9634b59a04714d6acaeb61
lin yan <1466030775@qq.com>
2024-07-05 10:49:19 +0800

    feat: 代码拆分、优化

--- src/views/aboutView/hooks.ts
+++ src/views/aboutView/hooks.ts
@@ -1,188 +1,193 @@
 import { handleOn, getInitFormProps } from './utils'
 import { v4 as uuidv4 } from 'uuid'
-import type { IConfigList, IFormLayout, IFormProps, IFormState, IItemContent } from './types'
+import type { IConfigList, IFormProps, IFormState, IItemContent, IItem } from './types'
 import type { Ref } from 'vue'
+const getModelKey = (component: string) => {
+  if (['checkbox', 'switch'].includes(component)) {
+    return 'checked'
+  }
+  return 'value'
+}
+
+function toNestedRef(obj: Record<string, any>, path: string) {
+  const keys = path.split('.')
+  const lastKey = keys.pop()!
+  const parent = keys.reduce((acc, key) => acc[key], obj)
+  const nestedRef = ref(parent[lastKey])
+  watch(nestedRef, (newValue) => {
+    parent[lastKey] = newValue
+  })
+  watch(
+    () => parent[lastKey],
+    (newValue) => {
+      nestedRef.value = newValue
+    }
+  )
+  return nestedRef
+}
+const getComponent = (option: IItem, obj: Record<string, any>, key: string) => {
+  // const dataRef = toRef(obj, key)
+  const dataRef = toNestedRef(obj, key)
+  const { component, componentProps = {}, on = {} } = option
+  const modelKey = getModelKey(component)
+  const updateModelKey = 'update:' + modelKey
+  return {
+    ...option,
+    id: uuidv4(),
+    component: 'a-' + component,
+    componentProps: {
+      ...componentProps,
+      [modelKey]: dataRef
+    },
+    on: {
+      ...on,
+      [updateModelKey]: (val: number | string) => {
+        dataRef.value = val
+        if (option.on?.[updateModelKey]) {
+          option.on[updateModelKey](val)
+        }
+      }
+    }
+  }
+}
 export const useCompileFormConfigList = (formProps: IFormProps) => {
   const res: IConfigList[] = []
-  const labelCol = ref(formProps.labelCol.span)
-  res.push({
-    id: uuidv4(),
-    component: 'a-slider',
-    title: 'labelCol',
-    componentProps: {
-      value: labelCol,
-      min: 1,
-      max: 24
-    },
-    on: {
-      'update:value': (val: number) => {
-        labelCol.value = val
-        formProps.labelCol.span = val
+  const widthComponent = getComponent(
+    {
+      component: 'slider',
+      title: '宽度',
+      componentProps: {
+        min: 1,
+        max: 24
       }
-    }
-  })
-  const wrapperCol = ref(formProps.wrapperCol.span)
-  res.push({
-    id: uuidv4(),
-    component: 'a-slider',
-    title: 'wrapperCol',
-    componentProps: {
-      value: wrapperCol,
-      min: 1,
-      max: 24
     },
-    on: {
-      'update:value': (val: number) => {
-        wrapperCol.value = val
-        formProps.wrapperCol.span = val
+    formProps,
+    'labelCol.span'
+  )
+  const wrapperColSpanComponent = getComponent(
+    {
+      component: 'slider',
+      title: 'wrapperCol',
+      componentProps: {
+        min: 1,
+        max: 24
       }
-    }
-  })
-  const layout = ref(formProps.layout)
-  res.push({
-    id: uuidv4(),
-    component: 'a-select',
-    title: 'layout',
-    componentProps: {
-      value: layout,
-      options: [
-        {
-          value: 'horizontal',
-          label: 'horizontal'
-        },
-        {
-          value: 'vertical',
-          label: 'vertical'
-        },
-        {
-          value: 'inline',
-          label: 'inline'
-        }
-      ]
     },
-    on: {
-      'update:value': (val: IFormLayout) => {
-        layout.value = val
-        formProps.layout = val
+    formProps,
+    'wrapperCol.span'
+  )
+  const layoutComponent = getComponent(
+    {
+      component: 'select',
+      title: 'layout',
+      componentProps: {
+        options: [
+          {
+            value: 'horizontal',
+            label: 'horizontal'
+          },
+          {
+            value: 'vertical',
+            label: 'vertical'
+          },
+          {
+            value: 'inline',
+            label: 'inline'
+          }
+        ]
       }
-    }
-  })
+    },
+    formProps,
+    'layout'
+  )
+  res.push(widthComponent, wrapperColSpanComponent, layoutComponent)
+
   return ref(res.map((item) => handleOn(item) as IConfigList))
 }
 export const useCompileConfigList = () => {
   const configList = ref<IConfigList[]>([])
 
   const updateConfigList = (item: IItemContent) => {
+    console.log('updateConfigList', item)
     const res: IConfigList[] = []
-    const titleRef = ref(item.title)
-    res.push({
-      id: uuidv4(),
-      component: 'a-input',
-      title: '标题',
-      componentProps: {
-        placeholder: '请输入',
-        value: titleRef
-      },
-      on: {
-        'update:value': (val: string) => {
-          item.title = val
-          titleRef.value = val
-        }
-      }
-    })
-    const spanRef = ref(item.span)
-    res.push({
-      id: uuidv4(),
-      component: 'a-slider',
-      title: '栅格',
-      componentProps: {
-        value: spanRef,
-        min: 1,
-        max: 24
-      },
-      on: {
-        'update:value': (val: number) => {
-          item.span = val
-          spanRef.value = val
-        }
-      }
-    })
-    if (item.width) {
-      const widthRef = ref(item.width)
-      res.push({
-        id: uuidv4(),
-        component: 'a-slider',
-        title: '宽度',
+    const titleComponent = getComponent(
+      {
+        component: 'input',
+        title: '标题',
         componentProps: {
-          value: widthRef,
-          min: 100,
-          max: 500
-        },
-        on: {
-          'update:value': (val: number) => {
-            item.width = val
-            widthRef.value = val
-          }
+          placeholder: '请输入'
         }
-      })
+      },
+      item,
+      'title'
+    )
+    const gridComponent = getComponent(
+      {
+        component: 'slider',
+        title: '栅格',
+        componentProps: {
+          min: 1,
+          max: 24
+        }
+      },
+      item,
+      'span'
+    )
+    res.push(titleComponent, gridComponent)
+    if (item.width) {
+      const widthComponent = getComponent(
+        {
+          component: 'slider',
+          title: '宽度',
+          componentProps: {
+            min: 100,
+            max: 500
+          }
+        },
+        item,
+        'width'
+      )
+      res.push(widthComponent)
     }
 
     if (item.slot) {
-      const slotRef = ref(item.slot)
-      res.push({
-        id: uuidv4(),
-        component: 'a-input',
-        title: '按钮文本',
-        componentProps: {
-          placeholder: '请输入',
-          value: slotRef
+      const slotComponent = getComponent(
+        {
+          component: 'input',
+          title: '按钮文本'
         },
-        on: {
-          'update:value': (val: string) => {
-            item.slot = val
-            slotRef.value = val
-          }
-        }
-      })
+        item,
+        'slot'
+      )
+      res.push(slotComponent)
     }
     if (!item.componentProps) return
     const { placeholder, showCount } = item.componentProps
     if (placeholder) {
-      const placeholderRef = ref(item.componentProps!.placeholder)
-      res.push({
-        id: uuidv4(),
-        component: 'a-input',
-        title: 'placeholder',
-        componentProps: {
-          placeholder: '请输入',
-          value: placeholderRef
-        },
-        on: {
-          'update:value': (val: string) => {
-            item.componentProps!.placeholder = val
-            placeholderRef.value = val
+      const placeholderComponent = getComponent(
+        {
+          component: 'input',
+          title: 'placeholder',
+          componentProps: {
+            placeholder: '请输入'
           }
-        }
-      })
+        },
+        item,
+        'componentProps.placeholder'
+      )
+      res.push(placeholderComponent)
     }
     if (showCount) {
-      const showCountRef = ref(item.componentProps!.showCount)
-      res.push({
-        id: uuidv4(),
-        component: 'a-checkbox',
-        title: '显示数字',
-        componentProps: {
-          checked: showCountRef
+      const numberComponent = getComponent(
+        {
+          component: 'checkbox',
+          title: '显示数字'
         },
-        on: {
-          'update:checked': (val: boolean) => {
-            item.componentProps!.showCount = val
-            showCountRef.value = val
-          }
-        }
-      })
+        item,
+        'componentProps.showCount'
+      )
+      res.push(numberComponent)
     }
-
     configList.value = res.map((item) => handleOn(item)) as IConfigList[]
   }
   return {
